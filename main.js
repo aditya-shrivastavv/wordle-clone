786260705ca2924d861a79cd147b7d6f9303452e
adityashrivastav1 <aditya.shri9981@gmail.com>
2023-02-11 09:52:57 +0530

    all functions made + formatted

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const ANSWER_LENGTH = 5;
 
 async function init() {
   let currentGuess = "";
+  let currentRow = 0;
 
   document.addEventListener("keydown", function handleKeyPress(e) {
     const action = e.key;
@@ -15,20 +16,43 @@ async function init() {
     } else if (isLetter(action)) {
       addLetter(action.toUpperCase());
     } else {
-      // do nothing 
+      // do nothing
     }
   });
 
-  function addLetter (letter) {
+  function addLetter(letter) {
     if (currentGuess.length < ANSWER_LENGTH) {
       currentGuess += letter;
     } else {
-      currentGuess = currentGuess.substring(0, currentGuess.length-1) + letter;
+      // Replace the last letter.
+      currentGuess =
+        currentGuess.substring(0, currentGuess.length - 1) + letter;
     }
 
-    letters[currentGuess.length - 1].innerText = letter;
+    letters[ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText =
+      letter;
   }
 
+  async function commit() {
+    if (currentGuess.length !== ANSWER_LENGTH) {
+      // do nothing
+      return;
+    }
+
+    // TODO: validate the word
+
+    // TODO: marking as "correct", "close" or "wrong".
+
+    // TODO: did they win or lose
+
+    currentRow++;
+    currentGuess = "";
+  }
+
+  function backspace() {
+    currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+    letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = "";
+  }
 }
 
 function isLetter(letter) {
